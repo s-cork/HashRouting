e71ecdd0348cda167e88b42fa975beae5af2d6ee
mrcork <stucork@gmail.com>
2020-01-29 06:24:19 +0800

    logger show_log with join

--- client_code/_logging.py
+++ client_code/_logging.py
@@ -1,31 +1,25 @@
 class Logger():
-  def __init__(self,debug, msg=''):
+  def __init__(self, debug, msg=''):
     self.debug = debug
-    self.msg   = msg
-    self._log  = msg
+    self.msg = msg
+    self._log = [(msg,)]
 
-  def print(self,*args,**kwargs):
+  def print(self, *args, **kwargs):
     if self.debug:
-      print(self.msg, *args,**kwargs) if self.msg else print(*args,**kwargs)
-    self._update_log(*args)
-  
+      print(self.msg, *args, **kwargs) if self.msg else print(*args, **kwargs)
+    self._log.append(args)
+
   def show_log(self):
-    return self._log
-    
-  def _update_log(self, *args):  
-    self._log += '\n' 
-    if len(args):
-      self._log += str(args[0]) + ' '
-    for arg in args[1:]:
-      self._log += str(arg) + ' '
-    
+    log_rows = [' '.join(str(arg) for arg in args) for args in self._log]
+    return '\n'.join(log_rows)
+
   @property
   def debug(self):
     return self._debug
 
   @debug.setter
-  def debug(self,value):
-    if not isinstance(value,bool):
+  def debug(self, value):
+    if not isinstance(value, bool):
       raise TypeError(f'debug should be a boolean, not {type(value).__name__}')
     self._debug = value
 
@@ -34,8 +28,8 @@ class Logger():
     return self._msg
 
   @msg.setter
-  def msg(self,value):
-    if not isinstance(value,str):
+  def msg(self, value):
+    if not isinstance(value, str):
       raise TypeError(f'msg should be type str, not {type(value).__name__}')
     self._msg = value
 
