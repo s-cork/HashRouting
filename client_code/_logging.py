ade90512bb9300b038286623ca27a528dc991458
mrcork <stucork@gmail.com>
2020-05-12 08:29:53 +0800

    Add licence to client code

--- client_code/_logging.py
+++ client_code/_logging.py
@@ -1,3 +1,16 @@
+"""
+    HashRouting
+    Copyright 2020 Stu Cork
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        http://www.apache.org/licenses/LICENSE-2.0
+
+    Source code published at https://github.com/s-cork/HashRouting
+"""
+
 class Logger():
   def __init__(self, debug, msg=''):
     self.debug = debug
