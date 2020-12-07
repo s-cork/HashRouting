d8b21b1c95ee3c4b2ffc9396a2539f510dd20974
stucork@gmail.com <stucork@gmail.com>
2020-12-07 06:23:22 +0000

    Edited app config, _navigation, routing and _logging

--- client_code/_logging.py
+++ client_code/_logging.py
@@ -11,41 +11,42 @@
     Source code published at https://github.com/s-cork/HashRouting
 """
 
-class Logger():
-  def __init__(self, debug, msg=''):
-    self.debug = debug
-    self.msg = msg
-    self._log = [(msg,)]
 
-  def print(self, *args, **kwargs):
-    if self.debug:
-      print(self.msg, *args, **kwargs) if self.msg else print(*args, **kwargs)
-    self._log.append(args)
+class Logger:
+    def __init__(self, debug, msg=""):
+        self.debug = debug
+        self.msg = msg
+        self._log = [(msg,)]
 
-  def show_log(self):
-    log_rows = [' '.join(str(arg) for arg in args) for args in self._log]
-    return '\n'.join(log_rows)
+    def print(self, *args, **kwargs):
+        if self.debug:
+            print(self.msg, *args, **kwargs) if self.msg else print(*args, **kwargs)
+        self._log.append(args)
 
-  @property
-  def debug(self):
-    return self._debug
+    def show_log(self):
+        log_rows = [" ".join(str(arg) for arg in args) for args in self._log]
+        return "\n".join(log_rows)
 
-  @debug.setter
-  def debug(self, value):
-    if not isinstance(value, bool):
-      raise TypeError(f'debug should be a boolean, not {type(value).__name__}')
-    self._debug = value
+    @property
+    def debug(self):
+        return self._debug
 
-  @property
-  def msg(self):
-    return self._msg
+    @debug.setter
+    def debug(self, value):
+        if not isinstance(value, bool):
+            raise TypeError(f"debug should be a boolean, not {type(value).__name__}")
+        self._debug = value
 
-  @msg.setter
-  def msg(self, value):
-    if not isinstance(value, str):
-      raise TypeError(f'msg should be type str, not {type(value).__name__}')
-    self._msg = value
+    @property
+    def msg(self):
+        return self._msg
 
-      
+    @msg.setter
+    def msg(self, value):
+        if not isinstance(value, str):
+            raise TypeError(f"msg should be type str, not {type(value).__name__}")
+        self._msg = value
 
-logger = Logger(debug=False, msg='#routing:')  #set to false if you don't wish to debug. You can also - in your main form - do routing.logger.debug = False
+
+logger = Logger(debug=False, msg="#routing:")
+# set to false if you don't wish to debug. You can also - in your main form - do routing.logger.debug = False
