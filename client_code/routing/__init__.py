5899528ae7f7a69316fec42111cf733c48024f6c
mrcork <stucork@gmail.com>
2021-03-13 12:23:51 +0800

    Make routing a package

---
+++ client_code/routing/__init__.py
@@ -0,0 +1,14 @@
+"""
+    HashRouting
+    Copyright 2020 Stu Cork
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        http://www.apache.org/licenses/LICENSE-2.0
+
+    Source code published at https://github.com/s-cork/HashRouting
+"""
+
+from ._routing import *
