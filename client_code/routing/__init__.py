464afa18e7312ccdfb5f12d2e00b8bc8617a5218
Stu Cork <stu@anvil.works>
2021-10-29 10:17:36 +0100

    use explicit named exports for autocompleters benefit

--- client_code/routing/__init__.py
+++ client_code/routing/__init__.py
@@ -5,6 +5,27 @@
 #
 # This software is published at https://github.com/anvilistas/anvil-extras
 
-__version__ = "1.6.0"
+__version__ = "1.8.1"
 
-from ._routing import *
+from ._routing import (
+    add_to_cache,
+    clear_cache,
+    error_form,
+    get_cache,
+    get_url_components,
+    get_url_dict,
+    get_url_hash,
+    get_url_pattern,
+    go,
+    go_back,
+    load_error_form,
+    load_form,
+    logger,
+    main_router,
+    on_session_expired,
+    reload_page,
+    remove_from_cache,
+    route,
+    set_url_hash,
+    set_warning_before_app_unload,
+)
